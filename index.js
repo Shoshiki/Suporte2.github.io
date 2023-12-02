71365b091c0d3d8cc8f049265ab9122ef2ab12bd
Jgrporto26 <134015794+Shoshiki@users.noreply.github.com>
2023-12-01 21:37:30 -0300

    attt

---
+++ index.js
@@ -0,0 +1,87 @@
+
+
+function executar() {
+  let atednimento, valorAtendimento, divres, pnumero, phora, alerta;
+  atednimento = document.querySelector("input#tipoatendimento");
+  valorAtendimento = Number(atednimento.value);
+  if (valorAtendimento == 1) {
+    divres = document.querySelector("div#res");
+    divres.innerHTML = `
+    LED DA FONTE POE:<br>
+    NOME DO WIFI CONTINUA O MESMO:<br>
+    FEITO REBOOT:<br>
+    CABOS VERIFICADOS:<br>
+    TESTE COM CABO DIRETO NO COMPUTADOR PPOE:<br>
+    COMO FINALIZOU ESSE CHECK LIST:<br>
+    <br>
+    CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
+    CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>
+    CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`;
+  } 
+  else if (valorAtendimento == 2) {
+    divres = document.querySelector("div#res");
+
+    divres.innerHTML = `LUZES DA ONU:<br>
+    STATUS DA ONU:<br> 
+    FEITO TROCA DE TOMADA:<br> 
+    NOME DO WIFI CONTINUA O MESMO:<br>  
+    FEITO REBOOT:<br> 
+    CABOS VERIFICADOS:<br> 
+    ONU ANALISADA E REAPROVISIONADA:<br> 
+    COMO FINALIZOU ESSE CHECK LIST:<br> 
+    <br>
+    CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
+    CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>
+    CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`
+  } 
+  else if (valorAtendimento == 3) {
+    divres = document.querySelector("div#res");
+
+    divres.innerHTML = `
+    REALIZADO REINCIO DE CPE<br>
+    REALIZADO REINCIO DE ONU<br>
+    REALIZADO DESCONEXÃO PELO SISTEMA<br>
+    REALIZADO REBOOT DOS EQUIPAMENTOS<br>
+    <br>
+    ALTERADA LARGURA DE BANDA DA REDE 2.4 E 5G<br>
+    2.4 >> 20Mhz<br>
+    5G >> 80Mhz<br>
+    `
+  }
+  
+  else if (valorAtendimento == 4) {
+    pnumero = prompt("Numero de TELEFONE do cliente:");
+    phora = prompt("Horario do contato:");
+    divres = document.querySelector("div#res");
+
+    divres.innerHTML = `TENTATIVA DE CONTATO VIA LIGAÇÃO</br></br>
+
+    ${pnumero} XXXXXXX AS ${phora} (ANEXO)</br>
+
+    ENVIADO TEMPLATE VIA WHATSAPP AS XXX (ANEXO)`;
+  }
+
+    else if (valorAtendimento == 5) {
+      divres = document.querySelector("div#res");
+  
+      divres.innerHTML = `
+      O procedimento vai ser o seguinte:<br>
+      <br>
+      1º Irá retirar o cabo de rede conectado ao seu roteador e conectar direto ao computador.<br>
+      <br>
+      2º Após conectar irá abrir o menu iniciar e pesquisar por painel de controle.<br>
+      <br>
+      3º Irá clicar em rede e internet > central de rede e 
+      compartilhamento > configurar uma nova rede ou conexão > conectar-se à internet > banda larga pppoe.<br>
+      <br>
+      4º Irá digitar o usuário: xxxxxx e senha: xxxxxx e irá clicar em conectar<br>
+      <br>
+      5º Após conectar irá nos mandar uma foto do que apareceu.
+      `
+    }
+
+    else {
+    alerta = alert("Não econtrado");
+    alerta.style.color = "red";
+  }
+}
