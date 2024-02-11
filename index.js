1ecf090f7623472dd5255cd63c5246549c5c85a3
Jgrporto26 <134015794+Shoshiki@users.noreply.github.com>
2024-02-11 10:05:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,68 @@
 
 
-function executar() {
-  let atednimento, valorAtendimento, divres, pnumero, phora, alerta;
-  atednimento = document.querySelector("input#tipoatendimento");
-  valorAtendimento = Number(atednimento.value);
-  if (valorAtendimento == 1) {
-    divres = document.querySelector("div#res");
-    divres.innerHTML = `
-    LED DA FONTE POE:<br>
-    NOME DO WIFI CONTINUA O MESMO:<br>
-    FEITO REBOOT:<br>
-    CABOS VERIFICADOS:<br>
-    TESTE COM CABO DIRETO NO COMPUTADOR PPOE:<br>
-    COMO FINALIZOU ESSE CHECK LIST:<br>
-    <br>
-    CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
-    CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>
-    CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`;
-  } 
-  else if (valorAtendimento == 2) {
-    divres = document.querySelector("div#res");
+document.addEventListener('DOMContentLoaded', function () {
+  const options = ['Selecione uma opção','O.S FTTX', 'O.S FTTH', 'Envio SMS','Script Lentidão' , 'Conexão PPOE' , 'Desconto Não-Concedido'];
+  const dropdown = document.getElementById('dropdown');
+  const infoContainer = document.getElementById('infoContainer');
+  const infoMessage = document.getElementById('infoMessage');
 
-    divres.innerHTML = `LUZES DA ONU:<br>
-    STATUS DA ONU:<br> 
-    FEITO TROCA DE TOMADA:<br> 
-    NOME DO WIFI CONTINUA O MESMO:<br>  
-    FEITO REBOOT:<br> 
-    CABOS VERIFICADOS:<br> 
-    ONU ANALISADA E REAPROVISIONADA:<br> 
-    COMO FINALIZOU ESSE CHECK LIST:<br> 
-    <br>
-    CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
-    CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>
-    CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`
-  } 
-  
-  else if (valorAtendimento == 3) {
-    pnumero = prompt("Numero de TELEFONE do cliente:");
-    phora = prompt("Horario do contato:");
-    divres = document.querySelector("div#res");
+  options.forEach(function (option, index) {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.toLowerCase().replace(' ', '-');
+    optionElement.text = option;
 
-    divres.innerHTML = `TENTATIVA DE CONTATO VIA LIGAÇÃO</br></br>
-
-    ${pnumero} XXXXXXX AS ${phora} (ANEXO)</br>
-<br>
-    ENVIADO TEMPLATE VIA WHATSAPP AS XXX (ANEXO)`;
-  }
-
-      else if (valorAtendimento == 4) {
-    divres = document.querySelector("div#res");
-
-    divres.innerHTML = `
-    REALIZADO REINCIO DE CPE<br>
-    REALIZADO REINCIO DE ONU<br>
-    REALIZADO DESCONEXÃO PELO SISTEMA<br>
-    REALIZADO REBOOT DOS EQUIPAMENTOS<br>
-    <br>
-    ALTERADA LARGURA DE BANDA DA REDE 2.4 E 5G<br>
-    2.4 >> 20Mhz<br>
-    5G >> 80Mhz<br>
-    `
-  }
-    else if (valorAtendimento == 5) {
-      divres = document.querySelector("div#res");
-  
-      divres.innerHTML = `
-      O procedimento vai ser o seguinte:<br>
-      <br>
-      1º Irá retirar o cabo de rede conectado ao seu roteador e conectar direto ao computador.<br>
-      <br>
-      2º Após conectar irá abrir o menu iniciar e pesquisar por painel de controle.<br>
-      <br>
-      3º Irá clicar em rede e internet > central de rede e 
-      compartilhamento > configurar uma nova rede ou conexão > conectar-se à internet > banda larga pppoe.<br>
-      <br>
-      4º Irá digitar o usuário: xxxxxx e senha: xxxxxx e irá clicar em conectar<br>
-      <br>
-      5º Após conectar irá nos mandar uma foto do que apareceu.
-      `
+    // Desativa a primeira opção (índice 0)
+    if (index === 0) {
+      optionElement.disabled = true;
+      optionElement.selected = true;
     }
 
-  else if (valorAtendimento == 6) {
-    divres = document.querySelector("div#res");
+    dropdown.add(optionElement);
+  });
 
-    divres.innerHTML = 
-    `APÓS ANALISE, NÃO SERÁ CONCEDIDO DESCONTO CONFORME SOLICITADO NESSE PROTOCOLO<br>
-    <br>
-    MOTIVO DO DESCONTO NEGADO (DETALHADO)<br> 
-    <br>
-    SMS ENVIADO AS XX:XX (ANEXO.
-    `
-  }   
+  dropdown.addEventListener('change', function () {
+    infoContainer.classList.add('hidden'); // Ocultar a div por padrão
 
-    else {
-    alerta = alert("Não econtrado");
-    alerta.style.color = "red";
-  }
-}
+    if (dropdown.value === 'o.s-fttx') {
+      infoMessage.innerHTML = 'LED DA FONTE POE:<br>NOME DO WIFI CONTINUA O MESMO:<br>FEITO REBOOT:<br>CABOS VERIFICADOS:<br>TESTE COM CABO DIRETO NO COMPUTADOR PPOE:<br>COMO FINALIZOU ESSE CHECK LIST:<br><br>TELEFONES CONFIRMADOS<br>CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>';
+  
+    } 
+    else if (dropdown.value === 'envio-sms') {
+      let pnumero = prompt("Número de TELEFONE do cliente:");
+      let phora = prompt("Horário do contato:");
+      infoMessage.innerHTML = `TENTATIVA DE CONTATO VIA LIGAÇÃO<br><br>${pnumero} XXXXXXX AS ${phora} (ANEXO)<br>ENVIADO TEMPLATE VIA WHATSAPP AS XXX (ANEXO)`;
+    } 
+    else if (dropdown.value === 'o.s-ftth') {
+      infoMessage.innerHTML = `LUZES DA ONU:<br>STATUS DA ONU:<br>FEITO TROCA DE TOMADA:<br> 
+      NOME DO WIFI CONTINUA O MESMO:<br>  
+      FEITO REBOOT:<br> 
+      CABOS VERIFICADOS:<br> 
+      ONU ANALISADA E REAPROVISIONADA:<br> 
+      COMO FINALIZOU ESSE CHECK LIST:<br> 
+      <br>
+      TELEFONES CONFIRMADOS<br>
+      CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
+      CLIENTE CIENTE DA POSSIVEL TAXA R$50,00<br>
+      CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`;
+
+    } 
+    else if (dropdown.value === 'script-lentidão') {
+      infoMessage.innerHTML = `REALIZADO REINCIO DE CPE<br>REALIZADO REINCIO DE ONU<br>REALIZADO DESCONEXÃO PELO SISTEMA<br>REALIZADO REBOOT DOS EQUIPAMENTOS<br><br>ALTERADA LARGURA DE BANDA DA REDE 2.4 E 5G<br>2.4 >> 20Mhz<br>5G >> 80Mhz<br>`;
+    } 
+    else if (dropdown.value === 'conexão-ppoe') {
+      infoMessage.innerHTML = `O procedimento vai ser o seguinte:<br><br>1º Irá retirar o cabo de rede conectado ao seu roteador e conectar direto ao computador.<br><br>2º Após conectar irá abrir o menu iniciar e pesquisar por painel de controle.<br><br>3º Irá clicar em rede e internet > central de rede e compartilhamento > configurar uma nova rede ou conexão > conectar-se à internet > banda larga pppoe.<br><br>4º Irá digitar o usuário: xxxxxx e senha: xxxxxx e irá clicar em conectar<br><br>5º Após conectar irá nos mandar uma foto do que apareceu.`;
+    } 
+    else if (dropdown.value === 'desconto-não-concedido') {
+      infoMessage.innerHTML = `APÓS ANALISE, NÃO SERÁ CONCEDIDO DESCONTO CONFORME SOLICITADO NESSE PROTOCOLO<br><br>MOTIVO DO DESCONTO NEGADO (DETALHADO)<br><br>SMS ENVIADO AS XX:XX (ANEXO.`;
+    } 
+    else if (dropdown.value === '------') {
+      infoMessage.innerHTML = ``;
+    }
+
+    if (dropdown.value !== 'default') {
+      // Se uma opção válida for selecionada, exibir a div
+      infoContainer.classList.remove('hidden');
+    }
+  });
+});
+
