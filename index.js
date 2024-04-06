c22329172cdf2cdb3dc50a017ba9f8d229a2c7da
Jgrporto26 <134015794+Shoshiki@users.noreply.github.com>
2024-04-06 09:38:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
+// Cria um evento com a função de barra de seleção
 
 document.addEventListener('DOMContentLoaded', function () {
-  const options = ['Selecione uma opção','O.S FTTX', 'O.S FTTH', 'Envio SMS','Script Lentidão' , 'Conexão PPOE' , 'Desconto Não-Concedido'];
-  const dropdown = document.getElementById('dropdown');
-  const infoContainer = document.getElementById('infoContainer');
-  const infoMessage = document.getElementById('infoMessage');
+  const options = ['Selecione uma opção','O.S FTTX', 'O.S FTTH', 'Envio SMS','Script Lentidão' , 'Conexão PPOE' , 'Desconto Não-Concedido']; //Opções da barra de seleção
+  const dropdown = document.getElementById('dropdown'); //Puxa as informações da div dropdown
+  const textarea = document.getElementById('myTextarea'); // Puxa as informações do textarea
 
+  //Cria uma função de opções da barra
   options.forEach(function (option, index) {
     const optionElement = document.createElement('option');
     optionElement.value = option.toLowerCase().replace(' ', '-');
@@ -21,47 +21,38 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   dropdown.addEventListener('change', function () {
-    infoContainer.classList.add('hidden'); // Ocultar a div por padrão
+    textarea.classList.add('hidden'); // Ocultar o textarea por padrão
 
+    // opções da barra 
     if (dropdown.value === 'o.s-fttx') {
-      infoMessage.innerHTML = 'LED DA FONTE POE:<br>NOME DO WIFI CONTINUA O MESMO:<br>FEITO REBOOT:<br>CABOS VERIFICADOS:<br>TESTE COM CABO DIRETO NO COMPUTADOR PPOE:<br>COMO FINALIZOU ESSE CHECK LIST:<br><br>TELEFONES CONFIRMADOS<br>CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>CLIENTE CIENTE DA POSSIVEL TAXA DE VISITA IMPRODUTIVA R$50,00<br>CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>';
-  
+      textarea.value = 'LED DA FONTE POE:\nNOME DO WIFI CONTINUA O MESMO:\nFEITO REBOOT:\nCABOS VERIFICADOS:\nTESTE COM CABO DIRETO NO COMPUTADOR PPOE:\nCOMO FINALIZOU ESSE CHECK LIST:\n\nTELEFONES CONFIRMADOS\nCLIENTE CIENTE DO PRAZO DE [24/48] HORAS\nCLIENTE CIENTE DA POSSIVEL TAXA DE VISITA IMPRODUTIVA R$50,00\nCLIENTE CIENTE DE RETORNO CASO NORMALIZE\n';
     } 
     else if (dropdown.value === 'envio-sms') {
       let pnumero = prompt("Número de TELEFONE do cliente:");
       let phora = prompt("Horário do contato:");
-      infoMessage.innerHTML = `TENTATIVA DE CONTATO VIA LIGAÇÃO<br><br>${pnumero} XXXXXXX AS ${phora} (ANEXO)<br>ENVIADO TEMPLATE VIA WHATSAPP AS XXX (ANEXO)`;
+      textarea.value = `TENTATIVA DE CONTATO VIA LIGAÇÃO\n\n${pnumero} XXXXXXX AS ${phora} (ANEXO)\nENVIADO TEMPLATE VIA WHATSAPP AS XXX (ANEXO)`;
     } 
     else if (dropdown.value === 'o.s-ftth') {
-      infoMessage.innerHTML = `LUZES DA ONU:<br>STATUS DA ONU:<br>FEITO TROCA DE TOMADA:<br> 
-      NOME DO WIFI CONTINUA O MESMO:<br>  
-      FEITO REBOOT:<br> 
-      CABOS VERIFICADOS:<br> 
-      ONU ANALISADA E REAPROVISIONADA:<br> 
-      COMO FINALIZOU ESSE CHECK LIST:<br> 
-      <br>
-      TELEFONES CONFIRMADOS<br>
-      CLIENTE CIENTE DO PRAZO DE [24/48] HORAS<br>
-      CLIENTE CIENTE DA POSSIVEL TAXA DE VISITA IMPRODUTIVA R$50,00<br>
-      CLIENTE CIENTE DE RETORNO CASO NORMALIZE<br>`;
-
+      textarea.value = `LUZES DA ONU:\nSTATUS DA ONU:\nFEITO TROCA DE TOMADA:\nNOME DO WIFI CONTINUA O MESMO:\nFEITO REBOOT:\nCABOS VERIFICADOS:\nONU ANALISADA E REAPROVISIONADA:\nCOMO FINALIZOU ESSE CHECK LIST:\n\nTELEFONES CONFIRMADOS\nCLIENTE CIENTE DO PRAZO DE [24/48] HORAS\nCLIENTE CIENTE DA POSSIVEL TAXA DE VISITA IMPRODUTIVA R$50,00\nCLIENTE CIENTE DE RETORNO CASO NORMALIZE\n`;
     } 
     else if (dropdown.value === 'script-lentidão') {
-      infoMessage.innerHTML = `REALIZADO REINCIO DE CPE<br>REALIZADO REINCIO DE ONU<br>REALIZADO DESCONEXÃO PELO SISTEMA<br>REALIZADO REBOOT DOS EQUIPAMENTOS<br><br>ALTERADA LARGURA DE BANDA DA REDE 2.4 E 5G<br>2.4 >> 20Mhz<br>5G >> 80Mhz<br>`;
+      textarea.value = `REALIZADO REINCIO DE CPE\nREALIZADO REINCIO DE ONU\nREALIZADO DESCONEXÃO PELO SISTEMA\nREALIZADO REBOOT DOS EQUIPAMENTOS\n\nALTERADA LARGURA DE BANDA DA REDE 2.4 E 5G\n2.4 >> 20Mhz\n5G >> 80Mhz\n`;
     } 
     else if (dropdown.value === 'conexão-ppoe') {
-      infoMessage.innerHTML = `O procedimento vai ser o seguinte:<br><br>1º Irá retirar o cabo de rede conectado ao seu roteador e conectar direto ao computador.<br><br>2º Após conectar irá abrir o menu iniciar e pesquisar por painel de controle.<br><br>3º Irá clicar em rede e internet > central de rede e compartilhamento > configurar uma nova rede ou conexão > conectar-se à internet > banda larga pppoe.<br><br>4º Irá digitar o usuário: xxxxxx e senha: xxxxxx e irá clicar em conectar<br><br>5º Após conectar irá nos mandar uma foto do que apareceu.`;
+      textarea.value = `O procedimento vai ser o seguinte:\n\n1º Irá retirar o cabo de rede conectado ao seu roteador e conectar direto ao computador.\n\n2º Após conectar irá abrir o menu iniciar e pesquisar por painel de controle.\n\n3º Irá clicar em rede e internet > central de rede e compartilhamento > configurar uma nova rede ou conexão > conectar-se à internet > banda larga pppoe.\n\n4º Irá digitar o usuário: xxxxxx e senha: xxxxxx e irá clicar em conectar\n\n5º Após conectar irá nos mandar uma foto do que apareceu.`;
     } 
     else if (dropdown.value === 'desconto-não-concedido') {
-      infoMessage.innerHTML = `APÓS ANALISE, NÃO SERÁ CONCEDIDO DESCONTO CONFORME SOLICITADO NESSE PROTOCOLO<br><br>MOTIVO DO DESCONTO NEGADO (DETALHADO)<br><br>SMS ENVIADO AS XX:XX`;
+      textarea.value = `APÓS ANALISE, NÃO SERÁ CONCEDIDO DESCONTO CONFORME SOLICITADO NESSE PROTOCOLO\n\nMOTIVO DO DESCONTO NEGADO (DETALHADO)\n\nSMS ENVIADO AS XX:XX`;
     } 
     else if (dropdown.value === '------') {
-      infoMessage.innerHTML = ``;
+      textarea.value = ``;
     }
 
     if (dropdown.value !== 'default') {
-      // Se uma opção válida for selecionada, exibir a div
-      infoContainer.classList.remove('hidden');
+      // Se uma opção válida for selecionada, exibir o textarea
+      textarea.classList.remove('hidden');
     }
   });
 });
+
+
